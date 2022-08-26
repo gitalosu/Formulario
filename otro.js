5a12f97e1c651ae1be96a678494e3c54d56d226b
karen <kvlo1998@gmail.com>
2022-08-25 21:06:52 -0500

    nuevo cambio

---
+++ otro.js
@@ -0,0 +1,19 @@
+
+let Auto = {
+    puertas: "N/A",
+    motor: 125,
+    llantas: 2,
+}
+
+let pan = {
+    insumos: ,
+    proceso: ,
+}
+
+let insumos = {
+    nombre: "Harina" ,
+    marca: "",
+    medida: "",
+}
+
+let Autos = [{Auto}]
