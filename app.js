68c3192d7664b8434f6aadf6f35dcdbc04dee23a
liro5 <lidisrondon@yahoo.es>
2022-08-30 20:00:31 -0500

    Se agregaron comillas Sencillas

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ function agregarEstudiante() {
         for (let i = 0; i < estudiantes.length; i++) {
             let table = `<tr>
             <td scope='row'>`+estudiantes[i].nombre+`</td>
-            <td>"+estudiantes[i].apellido+"</td>
-            <td>"+estudiantes[i].ciudad+"</td>
-            <td>"+estudiantes[i].telefono+"</td>
-          "</tr>`;
+            <td>`+estudiantes[i].apellido+`</td>
+            <td>`+estudiantes[i].ciudad+`</td>
+            <td>`+estudiantes[i].telefono+`</td>
+          </tr>`;
             document.getElementById("listaEstudiantes").innerHTML += table; //estudiantes[i].nombre +" "+ estudiantes[i].apellido + ", de la ciudad de "+ estudiantes[i].ciudad + ", telefono: "+estudiantes[i].telefono+ "<br>";
         }
 
